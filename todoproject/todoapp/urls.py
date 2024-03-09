15ce5eb9c19bde82b0e01bf8b062c592e4f57648
AndreaGogrichiani <justthepumpkin@gmail.com>
2024-03-09 17:59:20 +0400

    feat: button redirect from main page to add-todo page

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home),
+    path('add_todo/', views.add_todo, name='add_todo')
 ]
--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from .models import Todo
 
 
 def home(request):
     todos = Todo.objects.all()
     return render(request, 'todoapp/home.html', {'todos': todos})
+
+def add_todo(request):
+    return HttpResponse("HI")
