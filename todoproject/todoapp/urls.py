f766b4d44c1ff08993b75fac7588bb1e47b803dd
AndreaGogrichiani <justthepumpkin@gmail.com>
2024-03-09 21:33:54 +0400

    add add-todo page

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home),
-    path('add_todo/', views.add_todo, name='add_todo')
+    path('add_todo/', views.add_todo, name='add_todo'),
+    path('submitpage/', views.submitpage),
 ]
--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
+from .forms import TodoForm
 
 
 def home(request):
@@ -8,4 +9,18 @@ def home(request):
     return render(request, 'todoapp/home.html', {'todos': todos})
 
 def add_todo(request):
-    return HttpResponse("HI")
+    submitted = False
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/submitpage/')
+    else:
+        form = TodoForm()
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'todoapp/add_todo.html', {'form': form, 'submitted': submitted})
+
+
+def submitpage(request):
+    return render(request, 'todoapp/submitpage.html')
