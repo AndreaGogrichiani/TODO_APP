1ceaa03d883e9a814a8695a6c7e9efde1b236a5d
AndreaGogrichiani <justthepumpkin@gmail.com>
2024-03-09 16:45:39 +0400

    add home page

--- todoproject/todoapp/models.py
+++ todoproject/todoapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.TextField(max_length=50)
+    description = models.TextField(max_length=200)
